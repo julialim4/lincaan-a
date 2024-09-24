821babce0f340a310bbde34422b18df31bfe428b
julialim4 <161314817+julialim4@users.noreply.github.com>
2024-09-24 11:49:02 -0300

    jyjy

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ function soma(num1, num2){
     return num1 + num2;
 }
 console.log(soma(2,3));
+
+function areaquadrada(lado){
+ return lado**2;
+}
+let areatrapezio=function( ){
+return
+}
+console.log( )
+
+
+
+}
