cac857c7b5e813d905ab8b527234745b68db67d5
julialim4 <161314817+julialim4@users.noreply.github.com>
2024-09-24 11:55:58 -0300

    s

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ console.log(soma(2,3));
 function areaquadrada(lado){
  return lado**2;
 }
+
+console.log(areaquadrada(6))
+
 let areatrapezio=function(B,b,h){
 return (B+b)*h/2;
 }
 
-console.log( areatrapezio(2,4,5))
+console.log(areatrapezio(2,4,5))
