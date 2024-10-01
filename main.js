ab283def02551d7b03b3579de21fc9f4714453ac
julialim4 <161314817+julialim4@users.noreply.github.com>
2024-10-01 12:13:17 -0300

    xcxc

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
-function soma(num1, num2){ 
-    return num1 + num2;
+let i=0;
+white(i<nomes.length){
+console.log(i);
+comsole.log(nomes[i]);
+i++;
 }
-console.log(soma(2,3));
-
-function areaquadrada(lado){
- return lado**2;
-}
-
-console.log(areaquadrada(6))
-
-let areatrapezio=function(B,b,h){
-return (B+b)*h/2;
-}
-
-console.log(areatrapezio(2,4,5))
-
-function valores(jul,ia,a){
-    return jul+ia+a/3
-}
-
-parseInt(prompt('colocar os 3 numeros ae'))
-    
-    valores(343,64,3))
